6ed9f6c834c31d5571fee8e6e0545df8e5f46656
Aleksey <140403460+ne6esisbka@users.noreply.github.com>
2023-08-22 17:50:53 +0300

    Add files via upload

---
+++ data_input.py
@@ -0,0 +1,89 @@
+
+
+def add_user():
+    data_contacts = ['Введите ФИО = ', 'Введите название организации = ', 'Введите рабочий телефон = ',
+                     'Введите телефон личный (сотовый) = ']
+    new_user = [input(i) for i in data_contacts]
+
+    with open("Contacts.txt", "+a", encoding="UTF-8") as file:
+        file.write(f"{new_user}\n")
+    return new_user
+
+
+def search_user(su):
+    """ Функция поиска контакта """
+
+    reading_file = open('Contacts.txt', 'r', encoding='UTF-8')
+    rf = reading_file.read()
+    result = []
+
+# разбиваем список по строчно
+    for i in rf.split('\n'):
+
+        if su.lower() in i.lower():     # проверяем введённые данные на присутствие в строке списка контактов
+            result.append(i)          # ели есть совпадение добавляем строку контакта в result
+
+        else:
+            pass                    # если нет, то ничегоне делаем
+
+# проверяем если в переменной result значения
+    if len(result) > 0:
+        return
+    else:
+        return (f"**********************************************************************\n"
+                f"Результат поиска контакта\n"
+                f"{{*Нет такого контакта*}}\n"
+                f"**********************************************************************\n")
+
+
+def correct_contact(cont):
+    """Функция редактирования контакта"""
+
+    res = 0
+    with open('Contacts.txt', 'r', encoding='UTF-8') as file1:
+        lines = file1.readlines()
+        result = []
+
+    for line in lines:
+
+        if cont in line:
+            res += 1
+            print(f"для редактирования выбран ={line}")
+            lines.remove(line)
+            add_cont = str(add_user())
+            lines.append(f'{add_cont}\n')
+            with open('Contacts.txt', 'w', encoding='UTF-8') as file2:
+                for i in lines:
+                    file2.write(i)
+            return 'Запись изменена!'
+
+    if res == 0:
+        return 'Неправильный ввод!'
+
+
+def user_delete(du):
+    """Функцияудаления пользователя"""
+
+    result = []
+    with open('Contacts.txt', 'r', encoding='UTF-8') as file1:
+        lines = file1.readlines()
+        result = []
+
+    for line in lines:
+
+        if du in line:
+            print(f"для удаления выбран ={line}"
+                  f"Введите да, если уверены\n"
+                  f"Введите нет, если отменить")
+            question = input('Что выберешь ?  ')
+            if question.lower() == 'да' or question.lower() == 'yes':
+                lines.remove(line)
+
+                with open('Contacts.txt', 'w', encoding='UTF-8') as file2:
+                    for i in lines:
+                        file2.write(i)
+                return 'Запись УДАЛЕНА!'
+            elif question.lower() == 'нет' or question.lower() == 'no':
+                return 'Вы сжалились'
+            else:
+                return 'Моя твоя не понимать! Попробуйсте заного!'
