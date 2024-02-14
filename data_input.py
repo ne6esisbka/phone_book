9aee721a3644a763e5acbfe360c42bc76b9a75d4
Aleksey <140403460+ne6esisbka@users.noreply.github.com>
2024-02-14 15:00:40 +0300

    Update data_input.py

--- data_input.py
+++ data_input.py
@@ -1,81 +1,192 @@
-def add_user():
-    data_contacts = ['Введите ФИО = ', 'Введите название организации = ', 'Введите рабочий телефон = ',
-                     'Введите телефон личный (сотовый) = ']
-    new_user = [input(i) for i in data_contacts]
-
-    with open("Contacts.txt", "+a", encoding="UTF-8") as file:
-        file.write(f"{new_user}\n")
-    return new_user
+import pprint
 
 
-def search_user(su):
-    """ Функция поиска контакта """
+class PhoneBook:
+    """
+        Класс телефонного справочника
+        Содержит:
+            список всех контактов
+            функции добавления контактов
+            поиска контактов
+            изменение контактов
+            удаления контактов
+    """
+    def __init__(self):
+        self.command: str = input("Введите команду: ")
 
-    reading_file = open('Contacts.txt', 'r', encoding='UTF-8')
-    rf = reading_file.read()
-    result = []
+    @staticmethod
+    def my_decor(func):
+        """Декоратор для функций вывода"""
+        def wrapper(*args):
+            print(f"\n˜”*°•.•°*”˜˜”*°•.•°*”˜˜”*°•.•°*”˜˜”*°•.•°*”˜˜”*°•.•°*”˜˜”*°•.•°*”˜\n")
+            print(func(*args))
+            print(f"\n˜”*°•.•°*”˜˜”*°•.•°*”˜˜”*°•.•°*”˜˜”*°•.•°*”˜˜”*°•.•°*”˜˜”*°•.•°*”˜\n")
+        return wrapper
 
-# разбиваем список по строчно
-    for i in rf.split('\n'):
-        if su.lower() in i.lower():
-            result.append(i)
+    @my_decor
+    def user_action(self):
+        """Функция ответа на команды введённые пользователем"""
 
+        match self.command:
+            case '1':
+                return self.show_book()
+            case '2':
+                return self.add_user()
+            case '3':
+                correct_cont: str = input('Введите Фамилию/Имя/Отчество/ для изменения: ')
+                return self.correct_contact(correct_cont)
+            case '4':
+                search_cont: str = input('Введите Фамилию/Имя/Отчество/организацию для поиска: ')
+                return self.search_user(search_cont)
+            case '5':
+                del_cont: str = input('Введите Фамилию/Имя/Отчество/организацию для УДАЛЕНИЯ: ')
+                return self.user_delete(del_cont)
+            case _:
+                return "ОШИБКА : Нет такой команды!"
 
-# проверяем если в переменной result значения
-    if len(result) > 0:
-        return result
-    else:
-        return (f"**********************************************************************\n"
-                f"Результат поиска контакта\n"
-                f"{{* Нет такого контакта *}}\n"
-                f"**********************************************************************\n")
+    @staticmethod
+    def add_user() -> list:
+        """Функция добавления контакта"""
 
+        new_entry_in_book: dict = {
+                    1: "Введите Ваше ФИО: ",
+                    2: "Введите организацию если есть : ",
+                    3: "Введите рабочий телефон: ",
+                    4: "Введите сотовый телефон: "
+                }
+        new_list_entry: list = []
+        for i in range(1, len(new_entry_in_book) + 1):
+            x: str = input(new_entry_in_book[i])
+            if x != '':
+                new_list_entry.append(x)
+            else:
+                new_list_entry.append('Отсутствует')
 
-def correct_contact(cont):
-    """Функция редактирования контакта"""
+        with open("Contacts.txt", "+a", encoding="UTF-8") as file:
+            file.write(', '.join(new_list_entry) + '\n')
+        return new_list_entry
 
-    res = 0
-    with open('Contacts.txt', 'r', encoding='UTF-8') as file1:
-        lines = file1.readlines()
+    @staticmethod
+    def show_book() -> str:
+        """ Показывает список записей справочника """
+        with open('Contacts.txt', 'r', encoding='UTF-8') as file:
+            read_lines: list = sorted(file.readlines())
+            count: int = 0
+            print(' ☜(˚▽˚)☞  Телефонный справочник  ☜(˚▽˚)☞  \n')
+            while len(read_lines) > count:
+                try:
+                    for i in range(5):
+                        if read_lines[i + count]:
+                            print("".join(read_lines[i + count]))
 
-    for line in lines:
+                    count += 5
+                    print('▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼')
+                    input("Следующая страница Enter ....\n")
 
-        if cont in line:
-            res += 1
-            print(f"для редактирования выбран ={line}")
-            lines.remove(line)
-            add_cont = str(add_user())
-            lines.append(f'{add_cont}\n')
-            with open('Contacts.txt', 'w', encoding='UTF-8') as file2:
-                for i in lines:
-                    file2.write(i)
-            return 'Запись изменена!'
+                except IndexError:
+                    print('\nБольше записей нет!\n'
+                          '▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄ ▀▄')
+                    break
+                except KeyboardInterrupt:
+                    print('Завершение просмотра справочника')
+        return '\nКонец справочника!'
 
-    if res == 0:
-        return 'Неправильный ввод!'
+    def search_user(self, request: str, comnd=True):
+        """ Функция поиска контакта """
 
+        with open('Contacts.txt', 'r', encoding='UTF-8') as file:
+            rf = file.read()
+        result: list = []
 
-def user_delete(du):
-    """Функция удаления пользователя"""
+        for i in rf.split('\n'):
+            for elem in i.split(','):
+                if request.lower() in elem.lower() and len(request) > 1:
+                    if i is not result:
+                        result.append(i + '\n')
 
-    with open('Contacts.txt', 'r', encoding='UTF-8') as file1:
-        lines = file1.readlines()
+        if result and comnd:
+            return '\n'.join(set(result))
+        elif result and not comnd:
+            return set(result)
+        
+        return (f"Результат поиска контакта\n"
+                f"      ¯\_(ツ)_/¯     \n"
+                f"* Нет такого контакта *\n")
 
-    for line in lines:
+    def correct_contact(self, request: str) -> str:
+        """Функция редактирования контакта"""
 
-        if du in line:
-            print(f"для удаления выбран ={line}"
-                  f"Введите да, если уверены\n"
-                  f"Введите нет, если отменить")
-            question = input('Что выберешь ?  ')
-            if question.lower() == 'да' or question.lower() == 'yes':
+        list_user = self.search_user(request, comnd=False)
+        if isinstance(list_user, set):
+            dict_user: dict = {}
+            key_du: int = 1
+            for st in list_user:
+                dict_user[key_du] = st.replace('\n', '')
+                key_du += 1
+            pprint.pprint(dict_user)
+        else:
+            return list_user
+
+        while True:
+            try:
+                choice_is_yours = int(input("Выберите цифру контакта для редактирования: "))
+                match 0 < choice_is_yours <= len(dict_user):
+                    case True: break
+                    case False: print('Неправильно выбран контакт')
+            except ValueError:
+                print("Неправильный ввод попробуйте ещё раз!")
+
+        with open('Contacts.txt', 'r', encoding='UTF-8') as file1:
+            lines = file1.readlines()
+
+        for line in lines:
+
+            if dict_user[choice_is_yours] in line:
+                print(f"для редактирования выбран = {line}")
+                
+                new_dict_user: list = []
+
+                for entry in dict_user[choice_is_yours].split(','):
+                    new_entry: str = input(f'Текущее {entry}  -  Enter если не изменять\nВведите новое значение : ')
+                    if new_entry != '':
+                        new_dict_user.append(new_entry)
+                    else:
+                        new_dict_user.append(entry)
+
                 lines.remove(line)
+                lines.append(', '.join(new_dict_user) + '\n')
 
                 with open('Contacts.txt', 'w', encoding='UTF-8') as file2:
                     for i in lines:
                         file2.write(i)
-                return 'Запись УДАЛЕНА!'
-            elif question.lower() == 'нет' or question.lower() == 'no':
-                return 'Вы сжалились'
-            else:
-                return 'Моя твоя не понимать! Попробуйте заново!'
+
+                return 'Запись изменена!'
+
+    @staticmethod
+    def user_delete(request: str) -> str:
+        """Функция удаления пользователя"""
+
+        with open('Contacts.txt', 'r', encoding='UTF-8') as file1:
+            lines = file1.readlines()
+
+        for line in lines:
+            
+            if request.lower() in line.lower():
+                print(f"для удаления выбран = {line}"
+                      f"Введите < да >, если уверены\n"
+                      f"Введите < нет >, если отменить")
+
+                while True:
+                    question: str = input('Что выберешь ?  ')
+                    match question.lower():
+                        case 'да' | 'yes' | 'y':
+                            lines.remove(line)
+                            with open('Contacts.txt', 'w', encoding='UTF-8') as file2:
+                                for i in lines:
+                                    file2.write(i)
+                            return '\nЗапись УДАЛЕНА!'
+
+                        case 'нет' | 'no':
+                            return 'Запись не удалена!'
+                        case _:
+                            print('Не правильный ввод')
